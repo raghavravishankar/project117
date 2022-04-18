cb2d2358d7258ff97d1c886e99f05b39dcfd42e5
raghavravishankar <92210619+raghavravishankar@users.noreply.github.com>
2022-04-18 21:29:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+console.log(quick_draw_data_set[random]);
+sketch = quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+
+timer_counter = 0;
+timer_check= "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
